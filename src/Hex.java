23b659590d4d22d9d5f13ff46ecae56490fd6e17
Gio <gpera008@fiu.edu>
2015-01-15 17:30:38 -0500

    Update Hex.java

--- src/Hex.java
+++ src/Hex.java
@@ -1,5 +1,12 @@
 public class Hex {
-
-
+  private String[] hex; // Array to hold hex value
+  // Takes in a string and ties it to class
+  public Hex(String hex) {
+    // TODO: Tie String to class
+  }
+  // Returns Hex String array
+  public String[] getHex() {
+    return null;
+  }
 
 }
