7652ce234fed5d3e1ad0b1b09c23dcb27b40e180
Gio <gpera008@fiu.edu>
2015-01-15 16:46:50 -0500

    Update BitString.java
    Dibs have been added!

--- src/BitString.java
+++ src/BitString.java
@@ -1,19 +1,19 @@
 public class BitString {
   // Array of bits
   private int[] bits;
-  // Takes in a bitstring and ties data to class
+  // Takes in a bitstring and ties data to class --> X HAS DIBS
   public BitString(String bitstring) {
     // TODO: Initialization logic
   }
-  // Returns BitString as an array
+  // Returns BitString as an array --> JONATHAN HAS DIBS
   public int[] getBitString() {
     return null;
   }
-  // Returns BitString as a Decimal
+  // Returns BitString as a Decimal --> GISELLE HAS DIBS
   public Decimal toDecimal() {
     return null;
   }
-  // Returns BitString as a String
+  // Returns BitString as a String --> HERNAN HAS DIBS
   @Override
   public String toString() {
     return null;
